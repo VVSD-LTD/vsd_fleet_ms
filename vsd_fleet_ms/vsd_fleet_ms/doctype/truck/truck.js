02f7930bcc6a7f146cc420066adfa84542dcafe3
vimalrathod82 <vimalrathod@vvsdtz.com>
2023-05-05 16:53:34 +0300

    Doctypes creation

---
+++ vsd_fleet_ms/vsd_fleet_ms/doctype/truck/truck.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, VV SYSTEMS DEVELOPER LTD and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Truck', {
+	// refresh: function(frm) {
+
+	// }
+});
