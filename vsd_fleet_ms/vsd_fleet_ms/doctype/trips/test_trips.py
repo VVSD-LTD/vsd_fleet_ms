c97523445a303eea8f0493cae8bc50d8f2df9d3c
vimalrathod82 <vimalrathod@vvsdtz.com>
2023-06-02 12:23:51 +0300

    created trip doctype

---
+++ vsd_fleet_ms/vsd_fleet_ms/doctype/trips/test_trips.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, VV SYSTEMS DEVELOPER LTD and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestTrips(FrappeTestCase):
+	pass
