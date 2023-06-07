63e06098463dca3ddb717428f6165e7bbab186eb
Malinza <ibrahim@vvsdtz.com>
2023-06-07 17:02:14 +0300

    added code to pull trip fixed expenses on route change

--- vsd_fleet_ms/vsd_fleet_ms/doctype/trips/trips.js
+++ vsd_fleet_ms/vsd_fleet_ms/doctype/trips/trips.js
@@ -3,43 +3,96 @@
 
 frappe.ui.form.on('Trips', {
 	refresh: function (frm) {
-		if (frm.doc.route && frm.doc.main_route_steps.length < 1 && frm.doc.docstatus == 0) {
-			frappe.model.with_doc('Trip Routes', frm.doc.route, function (frm) {
-				var reference_route = frappe.model.get_doc('Trip Routes', cur_frm.doc.route);
-				cur_frm.clear_table('main_route_steps');
-				reference_route.trip_steps.forEach(function (row) {
-					var new_row = cur_frm.add_child('main_route_steps');
-					new_row.location = row.location;
-					new_row.distance = row.distance;
-					new_row.fuel_consumption_qty = row.fuel_consumption_qty;
-					new_row.location_type = row.location_type;
-				});
-
-				cur_frm.refresh_field('main_route_steps');
-				cur_frm.dirty();
-				cur_frm.save();
-			});
-		}
-		var total_fuel = 0;
-		var total_distance = 0;
-		frm.doc.main_route_steps.forEach(function (row) {
-			total_fuel = total_fuel + parseInt(row.fuel_consumption_qty);
-			total_distance = total_distance + parseInt(row.distance);
-		});
-		frm.doc.side_trips.forEach(function (row) {
-			total_fuel = total_fuel + parseInt(row.total_fuel);
-			total_distance = total_distance + parseInt(row.total_distance);
-		});
-		if (frm.doc.total_fuel != total_fuel) {
-			frm.doc.total_fuel = total_fuel
-			frm.dirty();
-			frm.save();
-		}
-		if (frm.doc.total_distance != total_distance) {
-			frm.doc.total_distance = total_distance
-			frm.dirty();
-			frm.save();
-		}
+	// 	if (frm.doc.route && frm.doc.docstatus == 0 && frm.doc.main_route_steps.length < 1 ) {
+	// 		frappe.model.with_doc('Trip Routes', frm.doc.route, function (frm) {
+	// 			var reference_route = frappe.model.get_doc('Trip Routes', cur_frm.doc.route);
+	// 			cur_frm.clear_table('main_route_steps');
+	// 			reference_route.trip_steps.forEach(function (row) {
+	// 				var new_row = cur_frm.add_child('main_route_steps');
+	// 				new_row.location = row.location;
+	// 				new_row.distance = row.distance;
+	// 				new_row.fuel_consumption_qty = row.fuel_consumption_qty;
+	// 				new_row.location_type = row.location_type;
+	// 				cur_frm.refresh_field('main_route_steps');
+	// 			});
+	// 			cur_frm.clear_table('requested_fund_accounts_table');
+	// 			reference_route.fixed_expenses.forEach(function(row) {
+	// 				frappe.model.with_doc('Fixed Expenses', row.expense, function (frm) {
+	// 				var fixed_expense_doc = frappe.model.get_doc("Fixed Expenses", row.expense);
+	// 				var new_row = cur_frm.add_child('requested_fund_accounts_table');
+	// 				new_row.requested_date = frappe.datetime.nowdate();
+	// 				new_row.request_amount = row.amount;
+	// 				new_row.request_currency = row.currency;
+	// 				new_row.request_status = "Pre-Approved";
+	// 				new_row.expense_type = row.expense;
+	// 				new_row.expense_account = fixed_expense_doc.expense_account;
+	// 				new_row.payable_account = fixed_expense_doc.cash_bank_account;
+	// 				new_row.party_type = row.party_type;
+	// 				// if (row.party_type == "Employee" && cur_frm.doc.assigned_driver) {
+	// 				// 	new_row.party = frappe.db.get_value("Driver", cur_frm.doc.assigned_driver, "employee");
+	// 				// }
+	// 				cur_frm.refresh_field('requested_fund_accounts_table');
+	// 			})
+	// 		});	
+	// 	});
+	// 	cur_frm.dirty();
+	// 	cur_frm.save();
+	// }
+	// if (frm.doc.route && frm.doc.docstatus == 0 && frm.doc.requested_fund_accounts_table.length < 1 ) {
+	// 		frappe.model.with_doc('Trip Routes', frm.doc.route, function (frm) {
+	// 			var reference_route = frappe.model.get_doc('Trip Routes', cur_frm.doc.route);
+	// 			cur_frm.clear_table('main_route_steps');
+	// 			reference_route.trip_steps.forEach(function (row) {
+	// 				var new_row = cur_frm.add_child('main_route_steps');
+	// 				new_row.location = row.location;
+	// 				new_row.distance = row.distance;
+	// 				new_row.fuel_consumption_qty = row.fuel_consumption_qty;
+	// 				new_row.location_type = row.location_type;
+	// 				cur_frm.refresh_field('main_route_steps');
+	// 			});
+	// 			cur_frm.clear_table('requested_fund_accounts_table');
+	// 			reference_route.fixed_expenses.forEach(function(row) {
+	// 				frappe.model.with_doc('Fixed Expenses', row.expense, function (frm) {
+	// 				var fixed_expense_doc = frappe.model.get_doc("Fixed Expenses", row.expense);
+	// 				var new_row = cur_frm.add_child('requested_fund_accounts_table');
+	// 				new_row.requested_date = frappe.datetime.nowdate();
+	// 				new_row.request_amount = row.amount;
+	// 				new_row.request_currency = row.currency;
+	// 				new_row.request_status = "Pre-Approved";
+	// 				new_row.expense_type = row.expense;
+	// 				new_row.expense_account = fixed_expense_doc.expense_account;
+	// 				new_row.payable_account = fixed_expense_doc.cash_bank_account;
+	// 				new_row.party_type = row.party_type;
+	// 				// if (row.party_type == "Employee" && cur_frm.doc.assigned_driver) {
+	// 				// 	new_row.party = frappe.db.get_value("Driver", cur_frm.doc.assigned_driver, "employee");
+	// 				// }
+	// 				cur_frm.refresh_field('requested_fund_accounts_table');
+	// 			})
+	// 		});	
+	// 	});
+	// 	cur_frm.dirty();
+	// 	cur_frm.save();
+	// }
+		// var total_fuel = 0;
+		// var total_distance = 0;
+		// frm.doc.main_route_steps.forEach(function (row) {
+		// 	total_fuel = total_fuel + parseInt(row.fuel_consumption_qty);
+		// 	total_distance = total_distance + parseInt(row.distance);
+		// });
+		// frm.doc.side_trips.forEach(function (row) {
+		// 	total_fuel = total_fuel + parseInt(row.total_fuel);
+		// 	total_distance = total_distance + parseInt(row.total_distance);
+		// });
+		// if (frm.doc.total_fuel != total_fuel) {
+		// 	frm.doc.total_fuel = total_fuel
+		// 	frm.dirty();
+		// 	frm.save();
+		// }
+		// if (frm.doc.total_distance != total_distance) {
+		// 	frm.doc.total_distance = total_distance
+		// 	frm.dirty();
+		// 	frm.save();
+		// }
 	},
 	route: function (frm) {
 		frappe.model.with_doc('Trip Routes', frm.doc.route, function (frm) {
@@ -51,9 +104,27 @@ frappe.ui.form.on('Trips', {
 				new_row.distance = row.distance;
 				new_row.fuel_consumption_qty = row.fuel_consumption_qty;
 				new_row.location_type = row.location_type;
+				cur_frm.refresh_field('main_route_steps');
 			});
-
-			cur_frm.refresh_field('main_route_steps');
+			cur_frm.clear_table('requested_fund_accounts_table');
+        	reference_route.fixed_expenses.forEach(function(row) {
+				frappe.model.with_doc('Fixed Expenses', row.expense, function (frm) {
+				var fixed_expense_doc = frappe.model.get_doc("Fixed Expenses", row.expense);
+				var new_row = cur_frm.add_child('requested_fund_accounts_table');
+				new_row.requested_date = frappe.datetime.nowdate();
+				new_row.request_amount = row.amount;
+				new_row.request_currency = row.currency;
+				new_row.request_status = "Pre-Approved";
+				new_row.expense_type = row.expense;
+				new_row.expense_account = fixed_expense_doc.expense_account;
+				new_row.payable_account = fixed_expense_doc.cash_bank_account;
+				new_row.party_type = row.party_type;
+				// if (row.party_type == "Employee" && cur_frm.doc.assigned_driver) {
+				// 	new_row.party = frappe.db.get_value("Driver", cur_frm.doc.assigned_driver, "employee");
+				// }
+				cur_frm.refresh_field('requested_fund_accounts_table');
+			})
+        });		
 		});
 	},
 	side_trips_add: function (frm, cdt, cdn) {
@@ -129,4 +200,28 @@ frappe.ui.form.on('Truck Trip Location Update', {
 			win.focus();
 		}
 	}
+});
+frappe.ui.form.on('Requested Fund Details', {
+    disburse_funds: function (frm, cdt, cdn) {
+        frappe.msgprint("We are in Disburse funds");
+        if (frm.is_dirty()) {
+            frappe.throw(__("Plase Save First"));
+            return;
+        }
+        const row = locals[cdt][cdn];
+        if (row.journal_entry) return;
+        frappe.call({
+            method: "vsd_fleet_ms.vsd_fleet_ms.doctype.trips.trips.create_fund_jl",
+            args: {
+                doc: frm.doc,
+                row: row
+            },
+            callback: function (data) {
+                frm.reload_doc();
+                // frappe.set_route('Form', data.message.doctype, data.message.name);
+                const new_url = `${window.location.origin}/app/journal-entry/${data.message.name}`;
+                window.open(new_url, '_blank');
+            }
+        });
+    }
 });
--- vsd_fleet_ms/vsd_fleet_ms/doctype/trips/trips.py
+++ vsd_fleet_ms/vsd_fleet_ms/doctype/trips/trips.py
@@ -18,3 +18,89 @@ def create_vehicle_trip_from_manifest(args_array):
 		manifest.vehicle_trip = vehicle_trip.name
 		manifest.save()
 	return vehicle_trip.as_dict()
+
+@frappe.whitelist()
+def create_fund_jl(doc, row):
+    doc = frappe.get_doc(json.loads(doc))
+    row = frappe._dict(json.loads(row))
+    if row.journal_entry:
+        frappe.throw("Journal Entry Already Created")
+
+    if row.request_status != "Approved":
+        frappe.throw("Fund Request is not Approved")
+
+    accounts = []
+    company_currency = frappe.db.get_value(
+        "Company",
+        doc.company,
+        "default_currency",
+    )
+    frappe.msgprint(company_currency)
+    if company_currency != row.request_currency:
+        multi_currency = 1
+        exchange_rate = get_exchange_rate(row.request_currency, company_currency)
+    else:
+        multi_currency = 0
+        exchange_rate = 1
+
+    if row.request_currency != row.expense_account_currency:
+        debit_amount = row.request_amount * exchange_rate
+        debit_exchange_rate = exchange_rate
+    else:
+        debit_amount = row.request_amount
+        debit_exchange_rate = 1
+
+    if row.request_currency != row.payable_account_currency:
+        credit_amt = row.request_amount * exchange_rate
+        credit_exchange_rate = exchange_rate
+    else:
+        credit_amt = row.request_amount
+        credit_exchange_rate = 1
+
+    debit_row = dict(
+        account=row.expense_account,
+        exchange_rate=debit_exchange_rate,
+        debit_in_account_currency=debit_amount,
+        cost_center=row.cost_center,
+    )
+    accounts.append(debit_row)
+
+    credit_row = dict(
+        account=row.payable_account,
+        exchange_rate=credit_exchange_rate,
+        credit_in_account_currency=credit_amt,
+        cost_center=row.cost_center,
+    )
+    accounts.append(credit_row)
+
+    company = doc.company
+    user_remark = "Vehicle Trip No: {0}".format(doc.name)
+    if row.requested_date:
+        date = row.requested_date
+    else:
+        date = nowdate()
+        
+    jv_doc = frappe.get_doc(
+        dict(
+            doctype="Journal Entry",
+            posting_date=date,
+            accounts=accounts,
+            company=company,
+            multi_currency=multi_currency,
+            user_remark=user_remark,
+        )
+    )
+    jv_doc.flags.ignore_permissions = True
+    frappe.flags.ignore_account_permission = True
+    set_dimension(doc, jv_doc)
+    for account_row in jv_doc.accounts:
+        set_dimension(doc, jv_doc, tr_child=account_row)
+    jv_doc.save()
+    jv_url = frappe.utils.get_url_to_form(jv_doc.doctype, jv_doc.name)
+    si_msgprint = "Journal Entry Created <a href='{0}'>{1}</a>".format(
+        jv_url, jv_doc.name
+    )
+    frappe.msgprint(_(si_msgprint))
+    frappe.set_value(row.doctype, row.name, "journal_entry", jv_doc.name)
+    return jv_doc
+
