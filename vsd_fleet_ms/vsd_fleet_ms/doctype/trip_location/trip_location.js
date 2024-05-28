4a83efe45895e28d8b73b6561fea23e9fd0f775d
nelsonmpanju <nelsonnorbert87@gmail.com>
2024-05-28 16:04:03 +0300

    refactor: changing Trip Locations to Trip location

---
+++ vsd_fleet_ms/vsd_fleet_ms/doctype/trip_location/trip_location.js
@@ -0,0 +1,27 @@
+// Copyright (c) 2023, VV SYSTEMS DEVELOPER LTD and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Trip Location', {
+	// refresh: function(frm) {
+
+	// }
+    latitude(frm) {
+		geolocation(frm);
+    },
+    longitude(frm) {
+		geolocation(frm);
+    },
+    onload_post_render(frm) {
+		geolocation(frm);
+    }
+
+});
+function geolocation(frm){
+    if (frm.doc.latitude == 0 && frm.doc.longitude == 0) {
+        frm.fields_dict.location_latitude_longitude.map.setView([frm.doc.latitude, frm.doc.longitude], 13);
+    }
+    else {
+        frm.doc.latitude = frm.fields_dict.location_latitude_longitude.map.getCenter()['lat'];
+        frm.doc.longitude = frm.fields_dict.location_latitude_longitude.map.getCenter()['lng'];
+    }
+}
